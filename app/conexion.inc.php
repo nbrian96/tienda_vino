28ecd18e8a5a090c1ea23d8612a127754f3f04c7
Nelson Avila <33665807+nbrian96@users.noreply.github.com>
2021-11-23 17:17:40 -0300

    Update conexion.inc.php

--- app/conexion.inc.php
+++ app/conexion.inc.php
@@ -1,5 +1,4 @@
 <?php
-//$conexion = new mysqli("localhost", "host", "Fedefon2020@", "vino");    // DOMINIO
 $conexion = new mysqli("localhost", "root", "", "vino");    // LOCALHOST
 
 if ($conexion->connect_errno) {
